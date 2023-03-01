582ea1ab46a188ced1b1c351f13a5771b443639a
liuj <liuj@hdect.com.cn>
2023-03-01 15:25:36 +0800

    feat: init code

---
+++ src/test/java/com/lj/sercurity/SpringSercurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lj.sercurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSercurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
